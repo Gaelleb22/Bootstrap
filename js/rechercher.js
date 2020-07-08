acd5a4eb85238fe3e9149bf07a88127ca2eccaf1
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-07-08 15:41:10 +0200

    tp bootstrap étapes 1 à 12, essai de l'étape 13'

---
+++ js/rechercher.js
@@ -0,0 +1,8 @@
+function valideRecherche(){
+    var recherche = new String;
+    recherche = document.getElementById("recherche");
+    if(recherche = null){
+        document.write($('#myModal').modal('toggle'));
+    }
+
+}
